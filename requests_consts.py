28822a4768bf019cb6b736d2dd2c7d84240a5595
pauloamed <pauloaugusto99@ufrn.edu.br>
2020-06-02 09:38:57 -0300

    file for storing constants of POST requests

---
+++ requests_consts.py
@@ -0,0 +1,18 @@
+YEAR2PARAMS = {
+    '2017': {
+        'url': 'https://www.cmnat.rn.gov.br/vereadores/verbas',
+        'months_ids': ['0' + str(i) for i in range(3, 10)] + [str(i) for i in range(10, 13)],
+    },
+    '2018': {
+        'url': 'https://www.cmnat.rn.gov.br/vereadores/verbas',
+        'months_ids': [str(i) for i in range(13, 26)],
+    },
+    '2019': {
+        'url': 'https://www.cmnat.rn.gov.br/vereadoresnovo/verbas',
+        'months_ids': [str(i) for i in range(1, 13)],
+    },
+    '2020': {
+        'url': 'https://www.cmnat.rn.gov.br/vereadoresnovo/verbas',
+        'months_ids': [str(i) for i in range(13, 24)],
+    },
+}
